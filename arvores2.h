d1359c98a8166e5e4da1f05073eb50eb09612766
giuzis <giuliana.martins23@gmail.com>
2017-06-04 23:30:15 -0300

    Add all DSW files

---
+++ arvores2.h
@@ -0,0 +1,31 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+
+using namespace std;
+
+struct node
+{
+    int info;
+    node *left;
+    node *right;
+};
+
+node* newNode(int dado);
+int insere(node **no, int dado);
+void imprime(node* no, int i);
+void achaNULL (node* no);
+void mostraArvore(node* no, int n_elementos);
+void substitui(node* no, node* substituto);
+int remove(node** raiz, int dado);
+void criaEspinhaDorsal (node** raiz);
+void rotacaoDireita(node** raiz, node* avo, node* pai);
+void rotacaoEsquerda(node** raiz, node* avo, node** pai);
+//int rotacaoDireita (node* no);
+//void rotacaoEsquerda (node* no);
+void balanceiaArvore (node** raiz, int total);
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include "arvores2.h"
+
+int main()
+{
+    node *raiz = NULL;
+    int dado = 1, contador = 0, comando = 7;
+    system("clear");
+
+    while(comando)
+    {
+        cout << "Digite 1 para inserir um dado. " << endl;
+        cout << "Digite 2 para remover um dado. " << endl;
+        cout << "Digite 3 para imprimir a arvore. " << endl;
+        cout << "Digite 4 para criar espinha dorsal. " << endl;
+        cout << "Digite 0 para sair. " << endl;
+        cin >> comando;
+        switch (comando){
+        case 1:
+            //system("clear");
+            cout << "Digite o dado que quer inserir: ";
+            cin >> dado;
+            contador += insere(&raiz, dado);
+            break;
+        case 2:
+            //system("clear");
+            if(contador == 0)
+            {
+                cout << "Arvore vazia, nao ha o que remover. " << endl;
+                break;
+            }
+            cout << "Digite o dado que quer remover: ";
+            cin >> dado;
+            contador -= remove(&raiz, dado);
+            break;
+        case 3:
+            //system("clear");
+            mostraArvore(raiz, contador);
+            break;
+        case 4:
+            if(contador == 0)
+            {
+                cout << "Arvore vazia. " << endl;
+                break;
+            }
+            criaEspinhaDorsal(&raiz);
+            break;
+        case 5:
+            if(contador == 0)
+            {
+                cout << "Arvore vazia. " << endl;
+                break;
+            }
+            balanceiaArvore(&raiz, contador);
+            break;
+        case 0:
+            break;
+        default:
+            //system("clear");
+            cout << "Comando invalido! Tente novamente: ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
